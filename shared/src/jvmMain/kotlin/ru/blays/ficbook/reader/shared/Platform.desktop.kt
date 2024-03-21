394c304cdeb361fecb75337cbae5a36d0229596c
B1ays <tima.b.planshet@gmail.com>
2024-03-21 12:04:00 +0300

    Fix: Path to jvm cache folder

--- shared/src/jvmMain/kotlin/ru/blays/ficbook/reader/shared/Platform.desktop.kt
+++ shared/src/jvmMain/kotlin/ru/blays/ficbook/reader/shared/Platform.desktop.kt
@@ -1,4 +1,4 @@
 package ru.blays.ficbook.reader.shared
 //actual fun getPlatformName(): String = "Desktop ${System.getProperty("os.name")}"
 
-val filesDirPath = System.getProperty("user.dir") + "\\" + "cache" + "\\"
+val filesDirPath = "${System.getProperty("user.dir")}/cache/"
