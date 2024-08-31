64b860c9cd348e5771258c4e57cf243666078d3b
B1ays <tima.b.planshet@gmail.com>
2024-08-31 18:25:09 +0300

    Update dependencies & refactor build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,8 @@
-
-@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-    //trick: for the same plugin versions in all sub-modules
-    alias(libs.plugins.android.application).apply(false)
-    alias(libs.plugins.android.library).apply(false)
-    alias(libs.plugins.kotlin.android).apply(false)
-    alias(libs.plugins.kotlin.multiplatform).apply(false)
-    alias(libs.plugins.jetbrains.compose).apply(false)
-    alias(libs.plugins.com.github.ben.manes.versions)
-    alias(libs.plugins.nl.littlerobots.version.catalog.update)
-}
-
-
-buildscript {
-    repositories {
-        mavenCentral()
-    }
-}
-
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.multiplatform) apply false
+    alias(libs.plugins.jetbrains.compose) apply false
+    alias(libs.plugins.gradle.versions)
 }
